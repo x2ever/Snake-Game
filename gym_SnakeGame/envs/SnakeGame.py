295b95e44e14922786142aec75059b220d1998d7
Han ilseok <x2ever.han@gmail.com>
2021-03-08 18:32:44 +0900

    Fix bug

--- gym_SnakeGame/envs/SnakeGame.py
+++ gym_SnakeGame/envs/SnakeGame.py
@@ -82,12 +82,12 @@ class SnakeGameEnv(gym.Env):
             else:
                 self._update_tile()
         
-        return np.transpose(self.state, (2, 0, 1)), reward, done, {}
+        return np.transpose(np.copy(self.state, (2, 0, 1))), reward, done, {}
 
     def reset(self):
         self.__init__(size=self.size)
 
-        return np.transpose(self.state, (2, 0, 1))
+        return np.transpose(np.copy(self.state), (2, 0, 1))
 
     def render(self, mode='rgb_array'):
         img_size = 600
