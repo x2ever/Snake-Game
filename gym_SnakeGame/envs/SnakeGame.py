1224a3e1733c801ceb94e65d240493f6697ed583
Han ilseok <x2ever.han@gmail.com>
2021-03-09 15:34:54 +0900

    Fix bug

--- gym_SnakeGame/envs/SnakeGame.py
+++ gym_SnakeGame/envs/SnakeGame.py
@@ -60,7 +60,7 @@ class SnakeGameEnv(gym.Env):
             
             elif next_tile_type == 0:
                 self.length += 1
-                reward += 1
+                reward += len(self.snake)
                 self.without_reward = 0
                 self.snake.insert(0, (x, y))
                 self.need_new_tile = True
@@ -80,7 +80,7 @@ class SnakeGameEnv(gym.Env):
             else:
                 self._update_tile()
         
-        return np.copy(self.state), reward, done, {}
+        return np.copy(self.state), reward / (self.size ** 2 * (self.size ** 2 + 1)) , done, {}
 
     def reset(self):
         self.__init__(size=self.size)
